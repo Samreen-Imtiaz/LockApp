41ac882e78129394938d99337a30a3dc48e0c826
Adeel Javed <adj@quantumcph.com>
2016-10-30 19:45:51 +0500

    okay

--- app/src/main/java/test/screenlocker/com/myapplication/WelcomeActivity.java
+++ app/src/main/java/test/screenlocker/com/myapplication/WelcomeActivity.java
@@ -5,23 +5,23 @@ package test.screenlocker.com.myapplication;
  */
 
 
-        import android.content.Context;
-        import android.content.Intent;
-        import android.graphics.Color;
-        import android.os.Build;
-        import android.os.Bundle;
-        import android.support.v4.view.PagerAdapter;
-        import android.support.v4.view.ViewPager;
-        import android.support.v7.app.AppCompatActivity;
-        import android.text.Html;
-        import android.view.LayoutInflater;
-        import android.view.View;
-        import android.view.ViewGroup;
-        import android.view.Window;
-        import android.view.WindowManager;
-        import android.widget.Button;
-        import android.widget.LinearLayout;
-        import android.widget.TextView;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -32,7 +32,7 @@ public class WelcomeActivity extends AppCompatActivity {
     private int[] layouts;
     private Button btnSkip, btnNext;
     private PrefManager prefManager;
-
+//alee
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
